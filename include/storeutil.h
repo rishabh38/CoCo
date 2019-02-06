d46c16437ecc075cfdd3d6a1baa4076fe3ab5e2e
rishabh <onrishabh@gmail.com>
2019-02-06 23:00:05 +0530

    added readMultiBitsfromStore and writeMultiBitstoStore

--- include/storeutil.h
+++ include/storeutil.h
@@ -9,15 +9,21 @@
 int writeNumBitstoStore (store STORE, const uint64_t location,
                          const uint64_t wordStartBit, uint64_t number,
                          uint64_t length);
+int writeMulitBitstoStore (store STORE, const uint64_t location,
+                           const uint64_t wordStartBit, bool bitArray[],
+                           uint64_t length);
 uint64_t readNumBitstoStore (store STORE, const uint64_t location,
                              const uint64_t wordStartBit,
                              uint64_t bitLength);
+bool *readMultiBitsfromStore (const store STORE, const uint64_t location,
+                              const uint64_t wordStartBit,
+                              uint64_t numberofBits);
 uint64_t readNumBitsfromStore(store STORE, const uint64_t location,
                               const uint64_t wordStartBit, uint64_t width);
 int writeBytestoStore(store STORE, const uint64_t location,
                       const uint64_t wordStartBit, uint64_t number,
                       uint64_t byteLength, bool endianStyle);
-uint64_t readBytestoStore(const store STORE, const uint64_t location,
-                          const uint64_t wordStartBit, uint64_t byteLength,
-                          const bool endianStyle);
+uint64_t readBytesfromStore(const store STORE, const uint64_t location,
+                            const uint64_t wordStartBit, uint64_t byteLength,
+                            const bool endianStyle);
 #endif
